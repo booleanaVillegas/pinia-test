40014e0a9b872b0d9e6f7fc16118a76295a51ace
Juliana Maria Villegas Pelaez <1112106141@u.icesi.edu.co>
2022-09-06 19:26:08 -0500

    added products store (not working)

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,11 +3,14 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+
   const doubleCount = computed(() => count.value * 2)
+  const getCount = computed(() => count.value)
+
   function increment() {
     console.log('im being called')
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  return { count, doubleCount, getCount, increment }
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <script>
+  import { storeToRefs } from 'pinia'
 import { useCounterStore } from '../stores/counter'
+import { useProductsStore } from '../stores/products'
+
 
 export default {
   setup() {
     const counterStore = useCounterStore()
+    const productsStore = useProductsStore()
 
-    return { counterStore }
+
+    return { productsStore: { ...productsStore}, counterStore }
   },
   computed: {
     doubleCount() {
-      return this.counterStore.doubleCount
+     return this.counterStore.doubleCount
+    },
+    allProducts() {
+      console.log(this.productsStore)
+      return this.productsStore.getProducts
     }
   },
   methods: {
@@ -17,6 +26,11 @@ export default {
       this.counterStore.increment()
       console.log('New Count:', this.counterStore.count)
     },
+    createNewProduct(event) {
+      console.log(event.target.value)
+      this.productsStore.newProduct(event.target.value)
+      event.target.value = ''
+    }
   },
 }
 </script>
@@ -25,8 +39,13 @@ export default {
   <div class="about">
     <h1>This is an about page</h1>
     <h2>This is the counter</h2>
+    <p>{{ counterStore.getCount }}</p>
     <p>{{ doubleCount}}</p>
     <button @click="incrementAndPrint">Increment</button>
+
+    <h4 v-for="product in allProducts" :key="product">{{ product }}</h4>
+
+    <input type="text" name="Product" id="newProduct" @change="(e) => createNewProduct(e)">
   </div>
 </template>
 
