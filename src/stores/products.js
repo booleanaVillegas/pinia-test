c3f912f6ed46756bc3f79985b26b3f58977d5f35
Juliana Villegas <juliana.villegas@morningstar.com>
2022-09-10 23:40:03 -0500

    added array store workinggit add .

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,19 +1,13 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useProductsStore = defineStore('products', () => {
-  const products = ['juguete', 'fruta', 'casa']
-
-  const getProducts = computed(() =>  products)
-
-  function newProduct(product) {
-    products.push(product)
-    console.log(products)
-  }
-
-  function getSingleProduct(id) {
-    products.filter((product)=> product == id)
-  }
-
-  return { products, getProducts, newProduct, getSingleProduct }
+export const useProductsStore = defineStore('products', {
+    state: () => ({ products: ['juguete', 'fruta', 'casa'] }),
+    getters: {
+        getProducts: (state) => [...state.products],
+    },
+    actions: {
+        newProduct(product) {
+            this.products.push(product)
+        },
+    },
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,21 @@
 <script>
-  import { mapStores, storeToRefs } from 'pinia'
+  import { mapStores, storeToRefs, mapState, mapActions } from 'pinia'
 import { useCounterStore } from '../stores/counter'
 import { useProductsStore } from '../stores/products'
 
 
 export default {
   computed: {
-    doubleCount() {
-     return this.counterStore.doubleCount
-    },
+    ...mapStores(useProductsStore),
     allProducts() {
-      console.log(this.productsStore)
       return this.productsStore.getProducts
-    },
-    ...mapStores(useProductsStore, useCounterStore)
+    }
   },
   methods: {
-    incrementAndPrint() {
-      this.counterStore.increment()
-      console.log('New Count:', this.counterStore.count)
-    },
     createNewProduct(event) {
       console.log(event.target.value)
       this.productsStore.newProduct(event.target.value)
-      console.log(this.productsStore.getProducts)
+      console.log(this.getProducts)
       event.target.value = ''
     }
   },
@@ -33,12 +25,8 @@ export default {
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h2>This is the counter</h2>
-    <p>{{ counterStore.getCount }}</p>
-    <p>{{ doubleCount}}</p>
-    <button @click="incrementAndPrint">Increment</button>
 
-    <h4 v-for="product in productsStore.getProducts" :key="product">{{ product }}</h4>
+    <h4 v-for="product in allProducts" :key="product">{{ product }}</h4>
 
     <input type="text" name="Product" id="newProduct" @change="(e) => createNewProduct(e)">
   </div>
